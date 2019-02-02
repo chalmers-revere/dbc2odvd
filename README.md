da12f2f97f01026982e2e0e2a7f5854b2d06a98c
Christian Berger <christian.berger@gu.se>
2019-02-02 20:42:59 +0100

    * Header-only generator
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ docker build -t dbc2odvd:latest -f Dockerfile.amd64
 ```
 docker run --rm -ti --net=host -v /file/to/a/file.dbc:/file.dbc dbc2odvd:latest cantools monitor /file.dbc
 ```
+
+## Generate a header-only library to decode/encode CAN frames
+Pre-condition: A .dbc file named `myFile.dbc` is present in the current working directory.
+This file will be generated into `myFile.h`.
+```
+docker run --rm -ti -v $PWD:/in -w /in dbc2odvd:latest generateHeaderOnly.sh myFile.dbc
+```
---
+++ generateHeaderOnly.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+WD=$PWD
+cd /tmp && rm -fr gen && mkdir gen && cd gen && \
+cantools generate_c_source $WD/$1 && \
+HEADER_FILE=$(ls *.h)
+SOURCE_FILE=$(ls *.c)
+cat $HEADER_FILE | sed -e 's/\(^[^\ \}\#\/\\].*_encode.*\)/inline\ \1/g;s/\(^[^\ \}\#\/\\].*_decode.*\)/inline\ \1/g;s/\(^[^\ \}\#\/\\].*_is_in_range.*\)/inline\ \1/g;s/\(^[^\ \}\#\/\\].*_pack.*\)/inline\ \1/g;s/\(^[^\ \}\#\/\\].*_unpack.*\)/inline\ \1/g' > $HEADER_FILE.mod && cat $SOURCE_FILE >> $HEADER_FILE.mod && cat $HEADER_FILE.mod > $WD/$HEADER_FILE
